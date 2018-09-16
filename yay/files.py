a3500676a816155fc85d92539505ec9c875f5646
Hes Siemelink <hsiemelink@xebialabs.com>
2018-09-16 12:11:46 -0700

    Adds 'Read file' handler

--- yay/files.py
+++ yay/files.py
@@ -4,10 +4,17 @@ from yay.util import *
 
 def write_file(data, variables):
     name = get_parameter(data, 'name')
-    content = get_parameter(data, 'content')
+    content = get_parameter(data, 'contents') # See https://english.stackexchange.com/questions/56831/file-content-vs-file-contents
 
     with open(name, 'w') as file:
         file.write(format_yaml(content))
 
+def read_file(data, variables):
+    contents = read_yaml_file(data)
+    if len(contents) == 1:
+        return contents[0]
+    return contents
+
 # Register tasks
 core.register('Write file', write_file)
+core.register('Read file', read_file)
