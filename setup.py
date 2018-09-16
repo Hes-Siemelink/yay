547ce0b4d7444607d461f5ff05abd36ad06ef4df
Hes Siemelink <hsiemelink@xebialabs.com>
2018-09-16 12:13:10 -0700

    Version 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yay",
-    version="0.1",
+    version="0.2",
     author="Hes Siemelink",
     author_email="author@example.com",
     description="YAML script",
