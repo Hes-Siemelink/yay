f2f56dda6952661c45341d8fd015106379ffdc27
Hes Siemelink <hsiemelink@xebialabs.com>
2018-09-25 18:08:02 +0200

    Adds dependencies to 'requests' and 'jsonpath_rw' to setup.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yay",
-    version="0.2",
+    version="0.3",
     author="Hes Siemelink",
     author_email="author@example.com",
     description="YAML script",
@@ -17,5 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+        'jsonpath_rw'
     ],
 )
