0fb5e49824a092e9378dd38c27d22d937c6c6170
Hes Siemelink <hsiemelink@xebialabs.com>
2018-10-10 16:36:19 +0200

    Includes pyyaml dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yay",
-    version="0.3",
+    version="0.4",
     author="Hes Siemelink",
     author_email="author@example.com",
     description="YAML script",
@@ -19,6 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
+        'pyyaml',
         'requests',
         'jsonpath_rw'
     ],
