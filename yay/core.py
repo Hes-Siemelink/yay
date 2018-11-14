112136ba6d5765450ff6e72d62d820b0966438ea
Hes Siemelink <hsiemelink@xebialabs.com>
2018-11-14 09:26:33 +0100

    Adds 'If' and 'Switch' tasks.

--- yay/core.py
+++ yay/core.py
@@ -31,8 +31,13 @@ def process_task(task, variables = {}):
 
 
 def invoke_handler(handler, data, variables):
-    for rawData in as_list(data):
-        result = invoke_single_handler(handler, rawData, variables)
+    for taskData in as_list(data):
+        try:
+            result = invoke_single_handler(handler, taskData, variables)
+        except FlowBreak as f:
+            result = f.result
+            break
+
     return result
 
 def invoke_single_handler(handler, rawData, variables):
@@ -41,6 +46,14 @@ def invoke_single_handler(handler, rawData, variables):
     if not result == None:
         variables[RESULT_VARIABLE] = result
     return result
+
+class FlowBreak(Exception):
+    def __init__(self, result = None):
+        self.result = result
+
+#
+# For each
+#
 
 def foreach(data, variables):
     actions = get_parameter(data, 'Do')
@@ -67,6 +80,99 @@ def get_foreach_variable(data):
         if variable == 'Do':
             continue
         return variable
+
+#
+# If
+#
+
+def if_statement(data, variables):
+    actions = get_parameter(data, 'Do')
+    term = parse_conditions(data)
+
+    if term.is_true():
+        invoke_handler(process_task, actions, variables)
+
+def switch_statement(data, variables):
+    actions = get_parameter(data, 'Do')
+    term = parse_conditions(data)
+
+    if term.is_true():
+        invoke_handler(process_task, actions, variables)
+        raise FlowBreak()
+
+def parse_conditions(data):
+    if 'item' in data:
+        item = get_parameter(data, 'item')
+
+        equals = data.get('equals')
+        if (equals):
+            return Equals(item, equals)
+
+        list = data.get('in')
+        if (list):
+            return Contains(item, list)
+
+        raise YayException("If with 'item' should have either 'equals' or 'in'.")
+
+    elif 'all' in data:
+        all = get_parameter(data, 'all')
+        list = [parse_conditions(item) for item in all]
+        return All(list)
+
+    elif 'any' in data:
+        any = get_parameter(data, 'any')
+        list = [parse_conditions(item) for item in any]
+        return Any(list)
+
+    else:
+        raise YayException("If needs 'item', 'all' or 'any'.")
+
+
+class Equals():
+
+    def __init__(self, item, equals):
+        self.item = item
+        self.equals = equals
+
+    def is_true(self):
+        return self.item == self.equals
+
+    def __repr__(self):
+        return f"{self.item} == {self.equals}"
+
+class Contains():
+
+    def __init__(self, item, list):
+        self.item = item
+        self.list = list
+
+    def is_true(self):
+        return self.item in self.list
+
+    def __repr__(self):
+        return f"{self.item} in {self.list}"
+
+class All():
+
+    def __init__(self, list):
+        self.list = list
+
+    def is_true(self):
+        return all([item.is_true() for item in self.list])
+
+    def __repr__(self):
+        return f"AND {self.list}"
+
+class Any():
+
+    def __init__(self, list):
+        self.list = list
+
+    def is_true(self):
+        return any([item.is_true() for item in self.list])
+
+    def __repr__(self):
+        return f"OR {self.list}"
 
 def noop(data, variables):
     pass
@@ -117,7 +223,7 @@ def join_single_variable(var, updates, variables):
     variables[var] = value
 
 #
-#
+# Replace
 #
 
 def replace_text(data, variables):
@@ -151,6 +257,8 @@ def register(type, handler):
 
 register('Do', process_task)
 register('For each', foreach)
+register('If', if_statement)
+register('Switch', switch_statement)
 register('Assert equals', assert_equals)
 
 register('Set', set_variable)
