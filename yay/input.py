a2117e6b2f60422717280993978b6b0590090298
Hes Siemelink <hsiemelink@xebialabs.com>
2019-01-04 17:41:42 +0100

    Adds 'Input' task to ask user for variables that are not set

--- yay/input.py
+++ yay/input.py
@@ -9,6 +9,21 @@ def ask_user(data, variables):
     answers = prompt([data])
     variables.update(answers)
 
+def check_input(data, variables):
+
+    for variable in data:
+        if variable in variables:
+            continue
+
+        query = {
+            'type': 'input',
+            'message': data[variable] + f' ({variable}):',
+            'name': variable
+        }
+        answers = prompt([query])
+        variables.update(answers)
+
 
 # Register tasks
 core.register('User Input', ask_user)
+core.register('Input', check_input)
