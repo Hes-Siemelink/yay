f92c3ab4cf8703a461fb7b1d8a902adf5ea2290d
Hes Siemelink <hsiemelink@xebialabs.com>
2019-08-22 14:58:43 +0200

    Sets version to 0.9-SNAPSHOT

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yay",
-    version="0.8",
+    version="0.9-SNAPSHOT",
     author="Hes Siemelink",
     author_email="author@example.com",
     description="YAML script",
