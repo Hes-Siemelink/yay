fb94e486c048b4663c87fb4178a00b1256414f20
Hes Siemelink <hsiemelink@xebialabs.com>
2020-05-13 11:54:15 +0100

    No longer leaks Python context into variables.

--- yay/script.py
+++ yay/script.py
@@ -1,6 +1,21 @@
+import copy
+
 from yay.runtime import command_handler
 
 
 @command_handler('Python')
 def execute_python_script(data, context):
-    exec(data, context.variables)
+
+    # Put variables in context
+    python_context = copy.deepcopy(context.variables)
+
+    # Run Python script
+    exec(data, python_context)
+
+    # Update variables with Python context
+    for key in context.variables:
+        if key in python_context:
+            context.variables[key] = python_context[key]
+    output = python_context.get('output')
+    if output:
+        context.variables['output'] = output
